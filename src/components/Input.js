35885e97dec455be74382b87a4afbb20693539e7
Christin Mikolai <mail@christinmikolai.de>
2021-02-06 18:48:10 +0100

    adjust structure for input field

--- src/components/Input.js
+++ src/components/Input.js
@@ -7,10 +7,14 @@ class Input extends React.Component {
     return (
       <div className="App-input">
         <form onSubmit={this.props.handleSubmit}>
-          <label>
-            Paste CSV:
-            <input type="text" value={this.props.value} onChange={this.props.handleChange} />
-          </label>
+          <label for="csv-input">Paste CSV below:</label>
+          <textarea
+            name="csv-input"
+            type="text"
+            rows="20"
+            cols=""
+            value={this.props.value}
+            onChange={this.props.handleChange} />
           <input type="submit" value="Submit" />
         </form>
       </div>
